75fc9b7c52a33564346af48976a31afb7c02c7f9
hayley <simmons.hayley15@icloud.com>
2022-06-30 15:18:21 -0500

    Navbar finished; NavLinks set up for authentication

--- lifetracker-ui/src/components/Navbar/Navbar.jsx
+++ lifetracker-ui/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,25 @@
 import * as React from "react"
 import "./Navbar.css"
+import {Link} from "react-router-dom"
+import NavLinks from "../Navbar/NavLinks.jsx"
+import Codepath_logo from "../../assets/Codepath.svg"
 
 export default function Navbar() {
     
+    return (
+        <nav className = "Navbar">
+            <div className = "content">
+
+                <div className = "logo">
+                    <Link to = "/">
+                        <img className = "logo" src = {Codepath_logo}/>
+                    </Link>
+                </div>
+
+                <NavLinks/>
+
+            </div>
+        </nav>
+    )
+
 }
