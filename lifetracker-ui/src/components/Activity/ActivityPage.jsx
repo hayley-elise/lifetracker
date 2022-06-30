d43b3599b3e27e3f9e570ea38c50bd0b6d196b0a
hayley <simmons.hayley15@icloud.com>
2022-06-30 17:20:40 -0500

    Set up ActivityPage component

--- lifetracker-ui/src/components/Activity/ActivityPage.jsx
+++ lifetracker-ui/src/components/Activity/ActivityPage.jsx
@@ -1,5 +1,19 @@
 import * as React from "react"
+import ActivityFeed from "./Feed/ActivityFeed"
+import ActivityContextProvider from "../contexts/activity.jsx"
+import Loading from "../Loading/Loading"
 
 export default function ActivityPage() {
     
+    return (
+        <div className = "activity-page">
+          {/* call useActivityContext hook */}
+          {/* if isProcessing is true, render Loading compoment */}
+          {/* <Loading/> */}
+
+          {/* if isProcessing is false, render ActivityFeed component */}
+          <ActivityFeed/>
+        </div>
+      )
+
 }
