d668367a9d9ae090b84f930f81a59b6fcc3a52b7
hayley <simmons.hayley15@icloud.com>
2022-06-30 17:01:23 -0500

    Added Sleep & Exercise components

---
+++ lifetracker-ui/src/components/Activity/Exercise/Exercise.jsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import "./Exercise.css"
+
+export default function Exercise() {
+    
+    return (
+        <div className = "exercise">
+            <h2> Go for a walk. </h2>
+        </div>
+    )
+
+}
---
+++ lifetracker-ui/src/components/Activity/Sleep/Sleep.jsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+import "./Sleep.css"
+
+export default function Sleep() {
+    
+    return (
+        <div className = "sleep">
+            <h2> Zzzzzz... </h2>
+        </div>
+    )
+
+}
