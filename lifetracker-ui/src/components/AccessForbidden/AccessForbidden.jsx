22f534a3ee144f23e08f8cf0181d8fb32d174e2b
hayley <simmons.hayley15@icloud.com>
2022-06-30 17:08:07 -0500

    Bare code for NotFound & AccessForbidden

--- lifetracker-ui/src/components/AccessForbidden/AccessForbidden.jsx
+++ lifetracker-ui/src/components/AccessForbidden/AccessForbidden.jsx
@@ -1,6 +1,17 @@
 import * as React from "react"
 import "./AccessForbidden.css"
+import {Link} from "react-router-dom"
 
 export default function AccessForbidden() {
     
+    return (
+        <div className = "access-forbidden">
+          <p> Access forbidden. </p>
+
+          <br/>
+
+          <Link to = "/"> Return to Home Page: </Link>
+        </div>
+    )
+
 }
--- lifetracker-ui/src/components/NotFound/NotFound.jsx
+++ lifetracker-ui/src/components/NotFound/NotFound.jsx
@@ -1,6 +1,17 @@
 import * as React from "react"
+import {Link} from "react-router-dom"
 import "./NotFound.css"
 
 export default function NotFound() {
     
+    return (
+        <div className = "not-found">
+          <p> Can't find what you were looking for? </p>
+
+          <br/>
+
+          <Link to = "/"> Return to Home Page: </Link>
+        </div>
+    )
+
 }
