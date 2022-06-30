42487901f535ad4561234c8a543b70dc1e360cb3
hayley <simmons.hayley15@icloud.com>
2022-06-30 15:21:54 -0500

    LandingPage finished

--- lifetracker-ui/src/components/Landing/LandingPage.jsx
+++ lifetracker-ui/src/components/Landing/LandingPage.jsx
@@ -1,6 +1,16 @@
 import * as React from "react"
 import "./LandingPage.css"
+import smartwatch from "../../assets/smartwatch.svg"
 
 export default function LandingPage() {
     
+    return (
+        <div className = "landing-page">
+            <div className = "hero"> 
+                <img className = "hero-img" src = {smartwatch}/>
+                <p className = "cta"> Helping you take back control of your life </p>
+            </div>
+        </div>
+      )
+
 }
