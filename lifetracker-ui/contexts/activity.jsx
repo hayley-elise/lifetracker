a65cef30222ec1b64c9d0ca9822436100e996aea
hayley <simmons.hayley15@icloud.com>
2022-06-30 14:08:09 -0500

    Added Activity & Nutrition context files

---
+++ lifetracker-ui/contexts/activity.jsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import {useEffect} from "react"
+
+
+const ActivityContext = React.createContext()
+
+export default function ActivityContextProvider(prop) {
+    // for later
+
+    return (
+        prop
+    )
+
+}
---
+++ lifetracker-ui/contexts/nutrition.jsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import {useEffect} from "react"
+
+
+const NutritionContext = React.createContext()
+
+export default function NutritionContextProvider(prop) {
+    // for later
+
+    return (
+        prop
+    )
+
+}
