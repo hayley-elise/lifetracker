9951fda0cc7f560b2dd22714093640523e9ea957
hayley <simmons.hayley15@icloud.com>
2022-06-30 13:42:39 -0500

    Imported components

--- lifetracker-ui/src/components/App/App.jsx
+++ lifetracker-ui/src/components/App/App.jsx
@@ -1,5 +1,14 @@
 import * as React from "react"
 import "./App.css"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Navbar from "../Navbar/Navbar"
+import LandingPage from "../LandingPage/LandingPage"
+import LoginPage from "../LoginPage/LoginPage"
+import RegistrationPage from "../RegistrationPage/RegistrationPage"
+import ActivityPage from "../ActivityPage/ActivityPage"
+import NutritionPage from "../NutritionPage/NutritionPage"
+import AccessForbidden from "../AccessForbidden/AccessForbidden"
+import NotFound from "../NotFound/NotFound"
 
 export default function App() {
   return (
