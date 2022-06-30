84e22e7a90e03cd0cef0bfca685eec4fccff230d
hayley <simmons.hayley15@icloud.com>
2022-06-30 18:12:56 -0500

    ActivityFeed + SummaryStat set up

--- lifetracker-ui/src/components/Activity/Feed/ActivityFeed.jsx
+++ lifetracker-ui/src/components/Activity/Feed/ActivityFeed.jsx
@@ -1,6 +1,44 @@
 import * as React from "react"
 import "./ActivityFeed.css"
+import SummaryStat from "./SummaryStat"
 
-export default function ActivityFeed() {
-    
+export default function ActivityFeed({totalCaloriesPerDay = [], avgCaloriesPerCategory = []}) {
+    const limit = avgCaloriesPerCategory.size < 6 ? avgCaloriesPerCategory.size : 6
+
+    for (let i = 0; i < limit; i++) {
+        avgCaloriesPerCategory.push([i])
+    }
+   
+
+    return (
+        <div className = "activity-feed">
+
+            <div className="per-category">
+                <h4> Average Calories Per Category: </h4>
+
+                {avgCaloriesPerCategory.map((calories) => {
+                    <SummaryStat 
+                        stat = {Math.round(calories * 10) / 10} 
+                        label = {calories}
+                        substat = {calories}  // pass category
+                        />
+                    })
+                }
+            </div>
+
+            <div className = "per-day">
+                <h4> Total Calories Per Day </h4>
+
+                {totalCaloriesPerDay.map((calories) => {
+                    <SummaryStat
+                        stat = {Math.floor(calories)}
+                        label = {calories}
+                        substat = {calories}  // pass date (dd/mm/yyyy)
+                    />
+                    })
+                }
+            </div>
+        </div>
+    )
+
 }
--- lifetracker-ui/src/components/Activity/Feed/SummaryStat.jsx
+++ lifetracker-ui/src/components/Activity/Feed/SummaryStat.jsx
@@ -1,5 +1,13 @@
 import * as React from "react"
 
-export default function SummaryStat() {
+export default function SummaryStat({stat, label, substat}) {
+
+    return (
+        <div className = "summary-stat">
+            <p className = "primary-statistic"> {stat} </p>
+            <p className = "stat-label"> {label.toString()} </p>
+            <p className = "secondary-statistic"> {substat} </p>
+        </div>
+    )
     
 }
