bf7a4fc9376aefb7b5f2432abda0112be8bd17ae
hayley <simmons.hayley15@icloud.com>
2022-07-01 01:58:29 -0500

    Update server.js

--- lifetracker-api/server.js
+++ lifetracker-api/server.js
@@ -1,6 +1,6 @@
 const app = require("./app")
 const {PORT} = require("./config")
 
-listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`)
+app.listen(PORT, () => {
+  console.log(`🚀 Server running: http://localhost:${PORT}`)
 })
