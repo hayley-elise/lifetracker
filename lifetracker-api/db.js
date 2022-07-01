b8340638a74b0906777606ada1d9528d87ca37dc
hayley <simmons.hayley15@icloud.com>
2022-07-01 01:19:18 -0500

    moved config

--- lifetracker-api/db.js
+++ lifetracker-api/db.js
@@ -1,5 +1,5 @@
 const {Client} =  require("pg")
-const {getDatabaseUri} = require("./Config/config")
+const {getDatabaseUri} = require("./config")
 require("colors")
 
 const db = new Client({connectionString: getDatabaseUri()})
--- lifetracker-api/server.js
+++ lifetracker-api/server.js
@@ -1,5 +1,5 @@
 const app = require("./app")
-const {PORT} = require("./Config/config")
+const {PORT} = require("./config")
 
 app.listen(PORT, function () {
   console.log(`🚀 Server running on http://localhost:${PORT}`)
