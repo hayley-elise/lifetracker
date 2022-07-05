bcf7e62351131d93696d60f1cf21728ef9f20645
hayley <simmons.hayley15@icloud.com>
2022-07-05 03:04:06 -0500

    Update config.js

--- lifetracker-api/config.js
+++ lifetracker-api/config.js
@@ -3,7 +3,7 @@ require("colors")
 
 const PORT = process.env.Port ? Number(process.env.PORT) : 3001
 const SECRET_KEY = process.env.SECRET_KEY || "ilovefoodsoverymuch"
-const BCRYPT_WORK_FACTOR = 13
+const BCRYPT_WORK_FACTOR = process.env.BCRYPT_WORK_FACTOR || 13
 
 function getDatabaseUri() {
     const dbUser = process.env.DATABASE_USER || "postgres"
