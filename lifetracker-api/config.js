a0fe3edebd71ff8a453f03aea93234fb0c43be95
hayley <simmons.hayley15@icloud.com>
2022-07-05 03:01:34 -0500

    Created token.js

--- lifetracker-api/config.js
+++ lifetracker-api/config.js
@@ -2,9 +2,8 @@ require("dotenv").config()
 require("colors")
 
 const PORT = process.env.Port ? Number(process.env.PORT) : 3001
-// const SECRET_KEY = "ilovefoodsoverymuch"
+const SECRET_KEY = process.env.SECRET_KEY || "ilovefoodsoverymuch"
 const BCRYPT_WORK_FACTOR = 13
-// const IS_TESTING = false
 
 function getDatabaseUri() {
     const dbUser = process.env.DATABASE_USER || "postgres"
@@ -12,7 +11,6 @@ function getDatabaseUri() {
     const dbHost = process.env.DATABASE_HOST || "localhost"
     const dbPort = process.env.DATABASE_PORT || 5432
     const dbName = process.env.DATABASE_NAME || "lifetracker"
-    // const dbTestName = process.env.DATABASE_TEST_NAME || "lifetracker_test"
 
     // if DATABASE_URL environment variable exists, use it; otherwise, create db connection string
     return process.env.DATABASE_URL || `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
@@ -26,5 +24,6 @@ console.log("Database URI: ".cyan.bold, getDatabaseUri().magenta.underline)
 module.exports = {
     PORT,
     BCRYPT_WORK_FACTOR,
+    SECRET_KEY,
     getDatabaseUri
 }
