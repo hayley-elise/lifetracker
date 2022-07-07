d2f87dba934bce7d4aa78668a5a824d478f99e75
hayley <71570421+hayley-elise@users.noreply.github.com>
2022-07-07 14:47:48 -0500

    Auth.jsx: useEffect + fetchUserFromToken

--- lifetracker-ui/contexts/auth.jsx
+++ lifetracker-ui/contexts/auth.jsx
@@ -1,4 +1,4 @@
-import {createContext, useState, useContext} from "react"
+import {createContext, useState, useContext, useEffect} from "react"
 
 const AuthContext = createContext(null)
 
@@ -7,6 +7,44 @@ export const AuthContextProvider = ({children}) => {
     const [initialized, setInitialized] = useState()
     const [isProcessing, setIsProcessing] = useState()
     const [error, setError] = useState()
+    // const [form, setForm] = useState({
+    //     username: "",
+    //     firstName: "", 
+    //     lastName: "", 
+    //     email: "", 
+    //     password: "", 
+    //     confirmedPassword: "",
+    // })
+
+    useEffect(() => {
+        const token = localStorage.getItem("lifetracker_token")
+        if (token) {
+            apiClient.setToken(token)
+            isProcessing = setIsProcessing(true)
+            error = setError(null)
+            fetchUserFromToken()
+        }
+
+        setIsProcessing(false)
+        setInitialized(true)
+    }, [])
+
+    async function fetchUserFromToken() {
+        const {data, error} = await apiClient.fetchUserFromToken()
+        if (error) {
+            setError((e) => ({...e, form: error}))
+            const message = error?.response?.data?.error?.message
+            setError((e) => ({...e, form: message ? String(message) : String(error)}))
+        }
+
+        if (data?.user) {
+            user = setUser(user)
+            error = setError(null)
+        }
+
+        return user
+    }
+
     const authValue = {user, setUser, initialized, setInitialized, isProcessing, setIsProcessing, error, setError}
 
     return (
