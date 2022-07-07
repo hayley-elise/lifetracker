18de02d1d6e5a7a5264a4b84c7b744c82f6b3f9d
hayley <71570421+hayley-elise@users.noreply.github.com>
2022-07-07 15:30:36 -0500

    Finished Auth.jsx; ready to import

--- lifetracker-ui/contexts/auth.jsx
+++ lifetracker-ui/contexts/auth.jsx
@@ -7,22 +7,25 @@ export const AuthContextProvider = ({children}) => {
     const [user, setUser] = useState()
     const [initialized, setInitialized] = useState()
     const [isProcessing, setIsProcessing] = useState()
+    const [loggedIn, setLoggedIn] = useState()
+    const setIsLoading = useState()
     const [error, setError] = useState()
-    // const [form, setForm] = useState({
-    //     username: "",
-    //     firstName: "", 
-    //     lastName: "", 
-    //     email: "", 
-    //     password: "", 
-    //     confirmedPassword: "",
-    // })
+    const form = useState({
+        username: "",
+        firstName: "", 
+        lastName: "", 
+        email: "", 
+        password: "", 
+        confirmedPassword: "",
+    })
+
 
     useEffect(() => {
         const token = localStorage.getItem("lifetracker_token")
         if (token) {
             apiClient.setToken(token)
-            isProcessing = setIsProcessing(true)
-            error = setError(null)
+            setIsProcessing(true)
+            setError(null)
             fetchUserFromToken()
         }
 
@@ -30,6 +33,8 @@ export const AuthContextProvider = ({children}) => {
         setInitialized(true)
     }, [])
 
+
+    // fetch user data from token
     async function fetchUserFromToken() {
         const {data, error} = await apiClient.fetchUserFromToken()
         if (error) {
@@ -39,14 +44,74 @@ export const AuthContextProvider = ({children}) => {
         }
 
         if (data?.user) {
-            user = setUser(user)
-            error = setError(null)
+            setUser(data.user)
+            setError(null)
         }
-
-        return user
     }
 
-    const authValue = {user, setUser, initialized, setInitialized, isProcessing, setIsProcessing, error, setError}
+
+    // login user
+    async function loginUser(e) {
+        e.preventDefault()
+        setIsLoading(true)
+        setError((e) => ({...e, form: null}))
+
+        const {data, error} = await apiClient.login({email: form.email, password: form.password})
+        if (error) {
+            setError((e) => ({...e, form: error}))
+            const message = error?.response?.data?.error?.message
+            setError((e) => ({...e, form: message ? String(message) : String(error)}))
+            setIsLoading(false)
+        }
+
+        if (data?.user) {
+            setUser(data.user)
+            apiClient.setToken(data.token)
+            setIsLoading(false)
+            setLoggedIn(true)
+        }
+    }
+
+
+    // signup user
+    async function signupUser(e) {
+        e.preventDefault()
+        setIsLoading(true)
+        setError((e) => ({...e, form: null}))
+
+        if (form.confirmedPassword !== form.password) {
+            setError((e) => ({...e, confirmedPassword: "Passwords do not match."}))
+            setIsLoading(false)
+            return
+        } else {
+            setError((e) => ({...e, confirmedPassword: null}))
+        }
+
+        const {data, error} = await apiClient.signup({username: form.username, firstName: form.firstName, lastName: form.lastName, email: form.email, password: form.password})
+        if (error) {
+            setError((e) => ({...e, form: error}))
+            const message = error?.response?.data?.error?.message
+            setError((e) => ({...e, form: message ? String(message) : String(error)}))
+            setIsLoading(false)
+        }
+
+        if (data?.user) {
+            setUser(data.user)
+            apiClient.setToken(data.token)
+            setIsLoading(false)
+            setLoggedIn(true)
+        }
+    }
+
+
+    // logout user
+    async function logoutUser() {
+        await apiClient.logout()
+        setLoggedIn(false)
+    }
+
+
+    const authValue = {user, setUser, initialized, setInitialized, isProcessing, setIsProcessing, error, setError, loggedIn, setLoggedIn}
 
     return (
         <AuthContext.Provider value = {authValue}>
