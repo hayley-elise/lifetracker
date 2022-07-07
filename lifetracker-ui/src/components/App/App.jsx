3dffb642694831b41dd2b525f396db02722a938f
hayley <71570421+hayley-elise@users.noreply.github.com>
2022-07-07 14:23:54 -0500

    Wrapped App component in AuthContext

--- lifetracker-ui/src/components/App/App.jsx
+++ lifetracker-ui/src/components/App/App.jsx
@@ -9,30 +9,31 @@ import ActivityPage from "../Activity/ActivityPage"
 import NutritionPage from "../Nutrition/NutritionPage"
 import AccessForbidden from "../AccessForbidden/AccessForbidden"
 import NotFound from "../NotFound/NotFound"
+import {AuthContextProvider} from "../../../contexts/auth"
 
-// export default function AppContainer() {
-//   return (
-//     <AuthContext.Provider>
-//       <App/>
-//     </AuthContext.Provider>
-//   )
-// }
+export default function AppContainer() {
+  return (
+    <AuthContextProvider>
+      <App/>
+    </AuthContextProvider>
+  )
+}
 
-export default function App() {
+function App() {
   return (
     <div className = "app">
       <React.Fragment>
         <BrowserRouter>
 
-          <Navbar/>
-          <Routes>
-            <Route path = "/" element = {<LandingPage/>}/>
-            <Route path = "/register" element = {<RegistrationPage/>}/>
-            <Route path = "/login" element = {<LoginPage/>}/>
-            <Route path = "/activity" element = {<ActivityPage/>}/>
-            <Route path = "/nutrition/*" element = {<NutritionPage/>}/>
-            <Route path = "*" element = {<NotFound/>}/>
-          </Routes>
+            <Navbar/>
+            <Routes>
+              <Route path = "/" element = {<LandingPage/>}/>
+              <Route path = "/register" element = {<RegistrationPage/>}/>
+              <Route path = "/login" element = {<LoginPage/>}/>
+              <Route path = "/activity" element = {<ActivityPage/>}/>
+              <Route path = "/nutrition/*" element = {<NutritionPage/>}/>
+              <Route path = "*" element = {<NotFound/>}/>
+            </Routes>
 
         </BrowserRouter>
       </React.Fragment>
